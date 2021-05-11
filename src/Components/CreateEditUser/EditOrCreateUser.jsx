01b5866e21d5f2bbdd214e38aa02c38164265a2e
VeljkoSimonovic <50739842+VeljkoSimonovic@users.noreply.github.com>
2021-05-11 15:48:10 +0200

    Added all the user entity fields to edit/create user with validations

--- src/Components/CreateEditUser/EditOrCreateUser.jsx
+++ src/Components/CreateEditUser/EditOrCreateUser.jsx
@@ -4,50 +4,78 @@ import { useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import { Typography, Button, Box } from "@material-ui/core";
-import { emailIsValid, phoneIsValid } from "../../Utils/validation";
-
-// {
-//   "id": 1,
-//   "name": "Leanne Graham",
-//   "username": "Bret",
-//   "email": "Sincere@april.biz",
-//   "address": {
-//     "street": "Kulas Light",
-//     "suite": "Apt. 556",
-//     "city": "Gwenborough",
-//     "zipcode": "92998-3874",
-//     "geo": {
-//       "lat": "-37.3159",
-//       "lng": "81.1496"
-//     }
-//   },
-//   "phone": "1-770-736-8031 x56442",
-//   "website": "hildegard.org",
-//   "company": {
-//     "name": "Romaguera-Crona",
-//     "catchPhrase": "Multi-layered client-server neural-net",
-//     "bs": "harness real-time e-markets"
-//   }
+import {
+  emailIsValid,
+  phoneIsValid,
+  idIsValid,
+  zipIsValid,
+  latIsValid,
+  lngIsValid,
+} from "../../Utils/validation";
 
 function EditOrCreateUser(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [userName, setUserName] = useState("");
+  const [id, setId] = useState("");
+  const [street, setStreet] = useState("");
+  const [suite, setSuite] = useState("");
+  const [city, setCity] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [geoLat, setGeoLat] = useState("");
+  const [geoLng, setGeoLng] = useState("");
+  const [website, setWebsite] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [catchPhrase, setCatchPhrase] = useState("");
+  const [bs, setBs] = useState("");
 
   const [redirect, setRedirect] = useState(false);
   const { data } = useContext(PropContext);
   const user = data.find((e) => props.match.params.id == e.id);
-  console.log(props.match.params.id);
+  console.log(latIsValid(geoLat), lngIsValid(geoLng));
   const editUser = () => {
-    if (emailIsValid(email) && phoneIsValid(phone) && name && userName) {
+    if (
+      emailIsValid(email) &&
+      phoneIsValid(phone) &&
+      idIsValid(id, data) &&
+      zipIsValid(zipCode) &&
+      latIsValid(geoLat) &&
+      lngIsValid(geoLng) &&
+      name &&
+      userName &&
+      street &&
+      suite &&
+      city &&
+      website &&
+      companyName &&
+      catchPhrase &&
+      bs
+    ) {
       fetch(`https://jsonplaceholder.typicode.com/users/${user.id}`, {
         method: "PATCH",
         body: JSON.stringify({
+          id: id,
           name: name,
-          email: email,
-          phone: phone,
           username: userName,
+          email: email,
+          address: {
+            street: street,
+            suite: suite,
+            city: city,
+            zipcode: zipCode,
+            geo: {
+              lat: geoLat,
+              lng: geoLng,
+            },
+          },
+          phone: phone,
+          website: website,
+          company: {
+            name: companyName,
+            catchPhrase: catchPhrase,
+            bs: bs,
+          },
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -56,18 +84,53 @@ function EditOrCreateUser(props) {
         if (response.ok) setRedirect(true);
         else alert(`request failed ${response.status}`);
       });
-    } else alert("All fields are required, or email/phone format incorrect");
+    } else
+      alert(
+        "Id must be unique, all fields are required, or incorrect field format"
+      );
   };
-
   const createUser = () => {
-    if (emailIsValid(email) && phoneIsValid(phone) && name && userName) {
+    if (
+      emailIsValid(email) &&
+      phoneIsValid(phone) &&
+      idIsValid(id, data) &&
+      zipIsValid(zipCode) &&
+      latIsValid(geoLat) &&
+      lngIsValid(geoLng) &&
+      name &&
+      userName &&
+      street &&
+      suite &&
+      city &&
+      website &&
+      companyName &&
+      catchPhrase &&
+      bs
+    ) {
       fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
         body: JSON.stringify({
+          id: id,
           name: name,
-          email: email,
-          phone: phone,
           username: userName,
+          email: email,
+          address: {
+            street: street,
+            suite: suite,
+            city: city,
+            zipcode: zipCode,
+            geo: {
+              lat: geoLat,
+              lng: geoLng,
+            },
+          },
+          phone: phone,
+          website: website,
+          company: {
+            name: companyName,
+            catchPhrase: catchPhrase,
+            bs: bs,
+          },
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -76,7 +139,10 @@ function EditOrCreateUser(props) {
         if (response.ok) setRedirect(true);
         else alert(`request failed ${response.status}`);
       });
-    } else alert("All fields are required, or email/phone format incorrect");
+    } else
+      alert(
+        "Id must be unique, all fields are required, or incorrect field format"
+      );
   };
 
   return redirect ? (
@@ -132,6 +198,138 @@ function EditOrCreateUser(props) {
           color="secondary"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Id</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          type="number"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Street</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={street}
+          onChange={(e) => setStreet(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Suite</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={suite}
+          onChange={(e) => setSuite(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">City</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Zip Code</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+          type="number"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Geo Lattitude</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={geoLat}
+          onChange={(e) => setGeoLat(e.target.value)}
+          type="number"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Geo Longitude</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={geoLng}
+          onChange={(e) => setGeoLng(e.target.value)}
+          type="number"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Website</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={website}
+          onChange={(e) => setWebsite(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Company name</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={companyName}
+          onChange={(e) => setCompanyName(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Company catchphrase</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={catchPhrase}
+          onChange={(e) => setCatchPhrase(e.target.value)}
+          type="text"
+          required
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h6">Business slogan</Typography>
+        <TextField
+          style={{ margin: "2px" }}
+          variant="outlined"
+          color="secondary"
+          value={bs}
+          onChange={(e) => setBs(e.target.value)}
           type="text"
           required
         />
--- src/Utils/validation.js
+++ src/Utils/validation.js
@@ -4,3 +4,13 @@ export const emailIsValid = (email) =>
   );
 export const phoneIsValid = (phone) =>
   /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone);
+
+export const zipIsValid = (zip) => /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zip);
+
+export const idIsValid = (id, data) => !data.some((e) => e.id == id);
+
+export const latIsValid = (lat) =>
+  /^(-?[1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?)$/.test(lat);
+
+export const lngIsValid = (lng) =>
+  /^(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,18})?|180(?:\.0{1,18})?)$/.test(lng);
